dbe293be4f8bde41ee0ccc87ee18cef6e3e7678f
Sumedh-R-Mundewadi <sumedhrm.gdsc@gmail.com>
2024-11-04 12:31:36 +0530

    Changes made in dsa3

--- dsAndApp/dsa3.c
+++ dsAndApp/dsa3.c
@@ -6,13 +6,22 @@ int pop();
 void palindrom();
 void display();
 
-int main(){
+int main()
+{
     int n, i;
 
     printf("Enter the number of stack elements: ");
     scanf("%d", &n);
 
-    while(1){
-        
+    while (1)
+    {
+        printf("1. Push an item");
+        printf("2. Pop an item");
+        printf("3. check Palindrome");
+        printf("4. Display Stack elements");
+        printf("5. Exit");
+        printf("Enter your choice: ");
+        scanf("%d", &n);
+        printf("\n");
     }
 }
